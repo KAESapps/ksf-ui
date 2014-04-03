9aa777bb7062e0c20757d7c7d4702252acec43f7
Sylvain Vuilliot <sylvain.vuilliot@kaes.fr>
2014-04-03 13:36:00 +0200

    HtmlContainer

--- src/layout/Flow.js
+++ src/layout/Flow.js
@@ -1,41 +1,11 @@
 define([
 	'compose',
-	'ksf/dom/WithSize',
-	'ksf/dom/Boundable',
+	'../base/HtmlContainer',
 ], function(
 	compose,
-	WithSize,
-	Boundable
+	HtmlContainer
 ){
-	return compose(WithSize, Boundable, function(content) {
-		this.domNode = document.createElement('div');
-		content && this._appendContent(content);
-	}, {
-		_appendContent: function(content) {
-			// TODO: comparer avec une implémentation basée sur documentFragment
-			content.forEach(function(child) {
-				this.domNode.appendChild(child.domNode);
-			}, this);
-		},
-		content: function(content) {
-			this.clear();
-			this._appendContent(content);
-		},
-		clear: function() {
-			while(this.domNode.lastChild) {
-				this.domNode.removeChild(this.domNode.lastChild);
-			}
-		},
-		add: function(child, index) {
-			this.domNode.insertBefore(child.domNode, this.domNode.children[index]);
-		},
-		remove: function(index) {
-			this.domNode.removeChild(this.domNode.children[index]);
-		},
-		move: function(from, to) {
-			var toNode = this.domNode.children[to],
-				fromNode = this.domNode.children[from];
-			this.domNode.insertBefore(fromNode, toNode);
-		}
+	return compose(HtmlContainer, {
+		_tag: 'div',
 	});
 });
