01f13b434f39ca411f2b817f7f65fe8ae15cad64
Sylvain Vuilliot <sylvain.vuilliot@kaes.fr>
2014-04-17 21:12:50 +0200

    Select widget

---
+++ src/widget/editable/base/Select.js
@@ -0,0 +1,52 @@
+define([
+	'compose',
+	'ksf/base/Evented'
+], function(
+	compose,
+	Evented
+){
+	return compose(Evented, function() {
+		this.domNode = document.createElement('select');
+		var self = this;
+		this.domNode.addEventListener('change', function() {
+			self._emit('input', self.domNode.value);
+			self._setNodeValue(self._value);
+		});
+	}, {
+		_value: undefined,
+		value: function(value) {
+			if (arguments.length > 0) {
+				this._value = value;
+				this._setNodeValue(value);
+			} else {
+				return this._value;
+			}
+		},
+		_setNodeValue: function(value) {
+			this.domNode.value = value;
+		},
+		onInput: function(cb) {
+			return this._on('input', cb);
+		},
+		options: function(options) {
+			var domNode = this.domNode;
+			options.forEach(function(valueLabel, i) {
+				var childrenLength = domNode.children.length;
+				var el = (i >= childrenLength) ? document.createElement('option') : domNode.children[i];
+				el.value = valueLabel[0];
+				el.label = valueLabel[1];
+				if (i >= childrenLength) {
+					domNode.appendChild(el);
+				}
+			});
+			var optionsLength;
+			while (domNode.children.length > optionsLength) {
+				domNode.removeChild(domNode.lastChild);
+			}
+			this._setNodeValue(this._value);
+		},
+		inDom: function() {
+			this._setNodeValue(this._value);
+		},
+	});
+});
---
+++ src/widget/editable/base/tests/Select.js
@@ -0,0 +1,46 @@
+define([
+	'intern!object',
+	'intern/chai!assert',
+	'../Select',
+	'dojo/on',
+], function(
+	registerSuite,
+	assert,
+	Select,
+	on
+) {
+	var cmp, observedInputs;
+	registerSuite({
+		beforeEach : function() {
+			cmp = new Select();
+			observedInputs = [];
+			cmp.on('input', function(value) {
+				observedInputs.push(value);
+			});
+			cmp.options([
+				['syv', "Sylvain"],
+				['ket', "Quentin"],
+				['toto', "Toto"],
+			]);
+			document.body.appendChild(cmp.domNode);
+			cmp.inDom();
+		},
+		"no init value": function() {
+			assert.equal(cmp.value(), undefined);
+			assert.equal(cmp.domNode.value, "");
+		},
+		"changing value programmatically": function() {
+			cmp.value("toto");
+			assert.equal(cmp.value(), "toto");
+			assert.equal(cmp.domNode.value, 'toto');
+			assert.deepEqual(observedInputs, [], "No input event when setting value");
+		},
+		"simulating user selection": function() {
+			cmp.domNode.value = "toto";
+			on.emit(cmp.domNode, 'change', {});
+
+			assert.equal(cmp.value(), undefined);
+			assert.deepEqual(observedInputs, ['toto']);
+		},
+	});
+});
