331d5ed26aa904f96b4b50c0a42c35cbb72406d9
Quentin <quentin.vuilliot@kaes.fr>
2014-05-14 10:42:44 +0200

    Implementation of inDom in HtmlContainer & Single layout

--- src/base/HtmlContainer.js
+++ src/base/HtmlContainer.js
@@ -9,6 +9,7 @@ define([
 ){
 	return compose(_WithSize, _Boundable, function(content) {
 		this.domNode = document.createElement(this._tag);
+		this._children = [];
 		content && this._appendContent(content);
 	}, {
 		_tag: 'div', // div par défaut
@@ -16,6 +17,7 @@ define([
 			// TODO: comparer avec une implémentation basée sur documentFragment
 			content.forEach(function(child) {
 				this.domNode.appendChild(child.domNode);
+				this._children.push(child);
 			}, this);
 		},
 		content: function(content) {
@@ -26,15 +28,32 @@ define([
 			while(this.domNode.lastChild) {
 				this.domNode.removeChild(this.domNode.lastChild);
 			}
+			this._children.forEach(function(child) {
+				child.inDom && child.inDom(false);
+			}, this);
+			this._children = [];
 		},
 		add: function(child, beforeChild) {
 			this.domNode.insertBefore(child.domNode, beforeChild && beforeChild.domNode);
+			this._children.push(child);
+			child.inDom && child.inDom(this._inDom);
 		},
 		remove: function(child) {
 			this.domNode.removeChild(child.domNode);
+			var index = this._children.indexOf(child);
+			if (index > -1) {
+				this._children.splice(index, 1);
+			}
+			child.inDom && child.inDom(false);
 		},
 		move: function(child, beforeChild) {
 			this.domNode.insertBefore(child.domNode, beforeChild && beforeChild.domNode);
+		},
+		inDom: function(inDom) {
+			this._children.forEach(function(child) {
+				child.inDom && child.inDom(inDom);
+			}, this);
+			this._inDom = inDom;
 		}
 	});
 });
--- src/layout/Single.js
+++ src/layout/Single.js
@@ -13,8 +13,17 @@ define([
 	}, {
 		content: function(content) {
 			this._child && this.domNode.removeChild(this._child.domNode);
+			this._childInDom(false);
 			this.domNode.appendChild(content.domNode);
 			this._child = content;
+			this._childInDom(this._inDom);
+		},
+		_childInDom: function(inDom) {
+			this._child && this._child.inDom && this._child.inDom(inDom);
+		},
+		inDom: function(inDom) {
+			this._childInDom(inDom);
+			this._inDom = inDom;
 		}
 	});
 });
