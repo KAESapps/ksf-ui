c5e60e1e9e2eea5f63a76a5bc61e107266eb5163
Quentin <quentin.vuilliot@kaes.fr>
2014-05-15 16:43:20 +0200

    Themed buttons

---
+++ src/widget/themed/base/Button.js
@@ -0,0 +1,17 @@
+define([
+	'compose',
+	'../../base/Button',
+	'ksf/dom/style/_WithStyle',
+	'ksf/dom/style/JSS',
+	'../styles/button'
+], function(
+	compose,
+	Button,
+	_WithStyle,
+	JSS,
+	style
+){
+	return compose(Button, _WithStyle, {
+		_style: new JSS(style)
+	});
+});
---
+++ src/widget/themed/styles/button.js
@@ -0,0 +1,8 @@
+define({
+	display: 'inline-block',
+	padding: '0.5ex 1ex',
+	borderRadius: '3px',
+	border: '1px solid #b3b3b3',
+	backgroundColor: '#f2f2f2',
+	cursor: 'pointer',
+});
