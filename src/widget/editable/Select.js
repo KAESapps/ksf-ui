65775abb33e45670cab209d84ed933a61aac4384
Sylvain Vuilliot <sylvain.vuilliot@kaes.fr>
2014-05-16 12:15:13 +0200

    allow changing options of editable/Select

--- src/widget/editable/Select.js
+++ src/widget/editable/Select.js
@@ -12,8 +12,12 @@ define([
 	return compose(_Composite, _WithAccessor, {
 		_rootFactory: function() {
 			return new Select();
+		},
+		options: function(options) {
+			// TODO: prendre comme 'options' un observable
+			this._root.options(options);
 		}
 	}, function(value, options) {
-		this._root.options(options.value());
+		options && this.options(options);
 	});
 });
