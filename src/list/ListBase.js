70ca7118f9d72c6a31133ae29b0186bcc1883da3
Sylvain Vuilliot <sylvain.vuilliot@kaes.fr>
2014-05-16 13:21:40 +0200

    update ListBase to the new API of HtmlContainer

--- src/list/ListBase.js
+++ src/list/ListBase.js
@@ -27,19 +27,20 @@ define([
 		add: function(item, index) {
 			index === undefined && (index = this._components.length);
 			var cmp = this._itemFactory(item);
-			this._root.add(cmp, index);
+			var beforeCmp = this._components[index];
+			this._root.add(cmp, beforeCmp);
 			this._components.splice(index, 0, cmp);
 			return cmp;
 		},
 		remove: function(index) {
-			this._root.remove(index);
-			var cmp = this._components.splice(index, 1);
-			destroy(cmp[0]);
+			var cmp = this._components.splice(index, 1)[0];
+			this._root.remove(cmp);
+			destroy(cmp);
 		},
 		move: function(from, to) {
-			this._root.move(from, to);
-			var cmp = this._components.splice(from, 1);
-			this._components.splice(to, 0, cmp[0]);
+			var fromCmp = this._components.splice(from, 1)[0];
+			var toCmp = this._components.splice(to, 0, fromCmp)[0];
+			this._root.move(fromCmp, toCmp);
 		},
 		clear: function() {
 			this._root.clear();
