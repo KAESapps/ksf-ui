4118aec0fb27e93eff02e592071e3b098d7bd9ce
Quentin <quentin.vuilliot@kaes.fr>
2014-06-05 16:11:21 +0200

    Label stylable

--- src/widget/base/Label.js
+++ src/widget/base/Label.js
@@ -1,11 +1,13 @@
 define([
 	'compose',
-	'ksf/dom/_WithSize'
+	'ksf/dom/_WithSize',
+	'ksf/dom/style/_Stylable'
 ], function(
 	compose,
-	_WithSize
+	_WithSize,
+	_Stylable
 ) {
-	return compose(_WithSize, function(label) {
+	return compose(_WithSize, _Stylable, function(label) {
 		this.domNode = document.createElement('span');
 		label && this.value(label);
 	}, {
