3d864aa31a02f7dea61b1f293239a25fde83e996
Sylvain Vuilliot <sylvain.vuilliot@kaes.fr>
2014-06-27 18:55:00 +0200

    use span in Labelled for styling purposes

--- src/widget/Labelled.js
+++ src/widget/Labelled.js
@@ -1,18 +1,23 @@
 define([
 	'compose',
-	'ksf/dom/_WithSize'
+	'ksf/dom/_WithSize',
 ], function(
 	compose,
 	_WithSize
 ) {
 	return compose(_WithSize, function(field, label) {
 		this.domNode = document.createElement('label');
-		this.domNode.textContent = label;
+		this._label = document.createElement('span');
+		this._label.textContent = label;
 		this._field = field;
+		this.domNode.appendChild(this._label);
 		this.domNode.appendChild(field.domNode);
 	}, {
 		inDom: function(inDom) {
 			this._field.inDom && this._field.inDom(inDom);
-		}
+		},
+		style: function(style) {
+			style.apply(this._label);
+		},
 	});
 });
--- src/widget/themed/Labelled.js
+++ src/widget/themed/Labelled.js
@@ -9,14 +9,14 @@ define([
 	_WithStyle,
 	JSS
 ){
-	return compose(Labelled, _WithStyle, {
-		_style: new JSS({
-			fontSize: 'smaller',
-			color: 'gray',
-			'> *': {
-				fontSize: 'larger',
-				display: 'block',
-			},
-		})
+	var labelStyle = new JSS({
+		fontSize: 'smaller',
+		color: 'gray',
+	});
+
+	return compose(Labelled, function() {
+		this.style(labelStyle);
+		// en théorie, il faudrait que le composant 'field' ait une API 'display' mais vu que cela n'aurait pas de valeur ajoutée, on se contente de dire que l'API est directement 'domNode.style.display', ce qui permet d'être compatible avec plus de composants
+		this._field.domNode.style.display = 'block';
 	});
 });
