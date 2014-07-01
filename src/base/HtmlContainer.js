f9a822d7e2d06b0546b1fb247dd8e7cc996b750f
Sylvain Vuilliot <sylvain.vuilliot@kaes.fr>
2014-07-01 09:17:00 +0200

    Chainable Label

--- src/base/HtmlContainer.js
+++ src/base/HtmlContainer.js
@@ -26,6 +26,7 @@ define([
 			if (arguments.length) {
 				this.clear();
 				this._appendContent(content);
+				return this;
 			} else {
 				return this._children;
 			}
--- src/widget/base/Label.js
+++ src/widget/base/Label.js
@@ -1,16 +1,18 @@
 define([
 	'compose',
 	'ksf/dom/_WithSize',
-	'ksf/dom/style/_Stylable'
+	'ksf/dom/style/_Stylable',
+	'ksf/base/Chainable',
 ], function(
 	compose,
 	_WithSize,
-	_Stylable
+	_Stylable,
+	Chainable
 ) {
 	return compose(function(label) {
 		this.domNode = document.createElement('span');
 		this.value(label);
-	}, _WithSize, _Stylable, {
+	}, _WithSize, _Stylable, Chainable, {
 		value: function(label) {
 			this.domNode.textContent = (label === undefined ? null : label);
 		}
