1e91a5c12324a1df9507e937751c32b1516f610b
Quentin <quentin.vuilliot@kaes.fr>
2014-08-22 19:20:12 +0200

    IconTextButton

---
+++ src/widget/base/IconTextButton.js
@@ -0,0 +1,33 @@
+define([
+	'compose',
+	'ksf/dom/composite/_Composite',
+	'ksf-ui/widget/base/_Clickable',
+	'ksf-ui/layout/Flow',
+	'ksf-ui/widget/base/Label',
+	'ksf/base/_Chainable'
+], function(
+	compose,
+	_Composite,
+	_Clickable,
+	Flow,
+	Label,
+	_Chainable
+){
+	return compose(_Composite, _Clickable, _Chainable, {
+		_rootFactory: function(iconChar, label) {
+			return new Flow({
+				display: 'inline-block',
+				verticalAlign: 'middle'
+			}).content([
+				this._own(new Label(iconChar), 'icon'),
+				this._own(new Label(label), 'text'),
+			]);
+		}
+	}, {
+		style: function(style) {
+			this._root.style(style.root);
+			this._owned.icon.style(style.icon);
+			this._owned.text.style(style.text);
+		}
+	});
+});
