7e571a7c0824eac109e91f3ca6b37c45a4984bd0
Sylvain <sylvain.vuilliot@kaes.fr>
2014-09-11 16:31:06 +0200

    do not use 'align: fit' by default in VFlex

--- src/layout/VFlex.js
+++ src/layout/VFlex.js
@@ -6,7 +6,7 @@ define([
 	_Flex
 ){
 	return compose(_Flex, {
-		_setChildPosition: function(child, childOptions) {
+		_setChildPosition: function(child) {
 			child.position({
 				mode: 'relative',
 				orientation: 'vertical'
@@ -14,24 +14,31 @@ define([
 		},
 		_layout: function() {
 			this._resetChildrenBounds();
-			
+
 			var self = this,
 				totalFixedHeight = 0,
 				innerSize = this._innerSize();
 			this._fixedChildren.forEach(function(childAndOptions) {
 				var child = childAndOptions[0];
+				var options = childAndOptions[1];
 				totalFixedHeight += child.size().height;
-				self._setChildBounds(child, {
-					width: innerSize.width
-				});
+				if (options.align === 'fit') {
+					self._setChildBounds(child, {
+						width: innerSize.width
+					});
+				}
 			});
 			var flexHeight = (innerSize.height - totalFixedHeight) / this._flexChildren.length;
 			this._flexChildren.forEach(function(childAndOptions) {
 				var child = childAndOptions[0];
-				self._setChildBounds(child, {
+				var options = childAndOptions[1];
+				var childBounds = {
 					height: flexHeight,
-					width: innerSize.width
-				});
+				};
+				if (options.align === 'fit') {
+					childBounds.width = innerSize.width;
+				}
+				self._setChildBounds(child, childBounds);
 			});
 		},
 	});
