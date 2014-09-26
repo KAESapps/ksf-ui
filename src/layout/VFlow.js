4b09ccb7b297128bf7f23e75058261d394f962b2
Quentin <quentin.vuilliot@kaes.fr>
2014-09-26 11:10:06 +0200

    Fixed VFlow

--- src/layout/VFlow.js
+++ src/layout/VFlow.js
@@ -26,32 +26,14 @@ define([
 			return new HtmlContainer();
 		}
 	}, _WithInnerSize, _RootStylable, function(options) {
-		this._posOptions = mixin({}, options, { display: 'block' });
-
-		this._own([], 'childrenBoundsCancelers');
+		this._posOptions = mixin({}, options, { mode: 'relative', orientation: 'vertical' });
 	}, {
 		add: function(child, beforeChild) {
 			this._setChildPosition(child);
 			this._root.add(child, beforeChild);
-			if (this.inDom()) {
-				var innerSize = this._innerSize();
-				this._setChildBounds(child, {
-					width: innerSize.width
-				});
-			}
 		},
 		remove: function(child) {
 			this._root.remove(child);
-		},
-		_setChildBounds: function(child, bounds) {
-			child.bounds(bounds);
-			this._owned.childrenBoundsCancelers.push(function() {
-				child.bounds(null);
-			});
-		},
-		_resetChildrenBounds: function() {
-			this._destroy('childrenBoundsCancelers');
-			this._own([], 'childrenBoundsCancelers');
 		},
 		content: function(content) {
 			var self = this;
@@ -59,42 +41,11 @@ define([
 				self._setChildPosition(child);
 			});
 			this._root.content(content);
-			this._layoutIfInDom();
 
 			return this;
-		},
-		inDom: function(inDom) {
-			var ret = _Composite.prototype.inDom.apply(this, arguments);
-			if (inDom) {
-				this._layout();
-			}
-			return ret;
-		},
-
-		_layoutIfInDom: function() {
-			if (this.inDom()) {
-				this._layout();
-			}
-		},
-
-		bounds: function() {
-			_Composite.prototype.bounds.apply(this, arguments);
-			this._layoutIfInDom();
 		},
 		_setChildPosition: function(child) {
 			child.position(this._posOptions);
 		},
-		_layout: function() {
-			this._resetChildrenBounds();
-
-			var self = this,
-				innerSize = this._innerSize();
-			this._root.content().forEach(function(child) {
-				self._setChildBounds(child, {
-					width: innerSize.width
-				});
-			});
-		},
-
 	});
 });
