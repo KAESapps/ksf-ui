a6d5eb5c13e82117ed05b2acf0d155cc4733d39f
Sylvain <sylvain.vuilliot@kaes.fr>
2014-10-06 09:48:51 +0200

    modifie le require-config pour être compatible avec la nouvelle organisation du code pour browserify

--- require-config.js
+++ require-config.js
@@ -1,7 +1,3 @@
-require.config({
-	packages: [{ name: 'ksf', location: 'ksf/src' }],
-	map: { 'ksf-ui': { compose: 'ksf/utils/compose' } }
-});
 
 if (typeof document !== "undefined") {
 	define(['ksf/require-config'], function() {});
