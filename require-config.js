115d7101024a847028f283e942fa1c76b6e818f7
Quentin <quentin.vuilliot@kaes.fr>
2014-10-07 11:22:06 +0200

    Fix AMD require-config

--- require-config.js
+++ require-config.js
@@ -1,3 +1,9 @@
+require({
+	packages: [
+		{ name: 'ksf', location: 'ksf' },
+		{ name: 'domready', location: 'domready', main: 'ready' },
+	],
+});
 
 if (typeof document !== "undefined") {
 	define(['ksf/require-config'], function() {});
