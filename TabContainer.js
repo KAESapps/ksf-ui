80e412938bea712828e0b1822ad5adbed520def2
Sylvain <sylvain.vuilliot@kaes.fr>
2014-10-07 15:18:02 +0200

    improve TabContainer

--- TabContainer.js
+++ TabContainer.js
@@ -14,48 +14,46 @@ define([
 	Button
 ) {
 	return compose(_Composite, function() {
-		this._setRoot(new VFlex());
+		this._setRoot(new VFlex().content([
+			this._own(new HFlex(), 'headerBar'),
+			[this._own(new Single(), 'body'), {flex: true}],
+		]));
 	}, {
+		_render: function() {
+			var content = this._content;
+			var style = this._style;
+			if (this._content && this._style) {
+				var activeTab = this._activeTab || content[0].key;
+				var bodyContent;
+				this._owned.headerBar.content(content.map(function(cmp) {
+					if (cmp.key === activeTab) {
+						bodyContent = cmp.body;
+					}
+					return new Button(cmp.header).chain('onAction', this.active.bind(this, cmp.key))
+						.style(activeTab === cmp.key ? style.header.activeTab : style.header.tabs);
+				}, this));
+				this._owned.body.content(bodyContent);
+			} else {
+				this._owned.headerBar.content([]);
+				this._owned.body.content(null);
+			}
+		},
 		content: function(content) {
 			this._content = content;
-			this._root.content([
-				this._own(new HFlex().content(content.map(function(cmp) {
-					return new Button(cmp.header).chain('onAction', this.active.bind(this, cmp.key));
-				}, this)), 'headerBar'),
-				[this._own(new Single(), 'body'), {flex: true}],
-			]);
-			this.active(content[0].key);
+			this._render();
 			return this;
 		},
 		active: function(tabKey) {
-			if (this._activeTab && this._style) {
-				this._activeTab.style(this._style.header.tabs);
-			}
-			var bodyContent;
-			this._content.some(function(contentElement, i) {
-				if (contentElement.key === tabKey) {
-					bodyContent = contentElement.body;
-					this._activeTab = this._owned.headerBar.content()[i];
-					return true;
-				}
-			}, this);
-			if (this._style) {
-				this._activeTab.style(this._style.header.activeTab);
-			}
-			this._owned.body.content(bodyContent);
+			this._activeTab = tabKey;
+			this._render();
 			return this;
 		},
 		style: function(style) {
 			this._style = style;
 			style.root && this._root.style(style.root);
 			style.body && this._owned.body.style(style.body);
-			if (style.header) {
-				style.header.root && this._owned.headerBar.style(style.header.root);
-				style.header.tabs && this._owned.headerBar.content().forEach(function(cmp) {
-					cmp.style(style.header.tabs);
-				});
-				style.header.activeTab && this._activeTab && this._activeTab.style(style.header.activeTab);
-			}
+			style.header && style.header.root && this._owned.headerBar.style(style.header.root);
+			this._render();
 			return this;
 		},
 	});
